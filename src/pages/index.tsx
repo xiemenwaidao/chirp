b9190ab95006187bedca0cefba3c4e426da75848
xiemenwaidao <menxielijiao@gmail.com>
2023-06-08 20:06:06 +0900

    🚧 font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ const PostView = (props: PostWithUser) => {
                     <span className="font-thin">・</span>
                     <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
                 </div>
-                <span>{post.content}</span>
+                <span className="text-2xl">{post.content}</span>
             </div>
         </div>
     );
