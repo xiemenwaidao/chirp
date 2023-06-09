3b9dd525627f28eb3e232de43dbf37920be49102
xiemenwaidao <menxielijiao@gmail.com>
2023-06-09 17:41:55 +0900

    🚧 Ratelimit the endpoint

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -8,6 +8,23 @@ const filterUserForClient = (user: User) => {
     return { id: user.id, username: user.username, profilePicture: user.profileImageUrl };
 };
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(3, "1 m"),
+    analytics: true,
+    /**
+     * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+     * instance with other applications and want to avoid key collisions. The default prefix is
+     * "@upstash/ratelimit"
+     * Redisで使用されるキーのオプションのプレフィックス。他のアプリケーションとRedisインスタンスを共有し、キーコリジョンを避けたい場合に便利です。デフォルトのプレフィックスは「@upstash/ratelimit」です。
+     */
+    //   prefix: "@upstash/ratelimit",
+});
+
 export const postRouter = createTRPCRouter({
     getAll: publicProcedure.query(async ({ ctx }) => {
         const posts = await ctx.prisma.post.findMany({
@@ -49,6 +66,9 @@ export const postRouter = createTRPCRouter({
         )
         .mutation(async ({ ctx, input }) => {
             const authorId = ctx.userId;
+
+            const { success } = await ratelimit.limit(authorId);
+            if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
             const post = await ctx.prisma.post.create({
                 data: {
